6f447e75611aa0c1988ced910e53b6778ab587ec
SMB <sethmichaelbrown@gmail.com>
2019-04-06 13:53:44 -0700

    Renaming migration, adding fields

---
+++ migrations/01_contributions.js
@@ -0,0 +1,13 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('contributions', function (table) {
+    table.increments('id')
+    table.string('fName', '40').notNullable()
+    table.string('lName', '40').notNullable()
+    table.string('email', '100').notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('contributions')
+}
